3a91e6f80c6b8e263fd9d31ea6d390ebc1ab032e
Tomoki Naito <tnaito@c6r1s4.42tokyo.jp>
2025-10-24 21:32:59 +0900

    fix \0 to \n

--- lib/rl_wrapper.c
+++ lib/rl_wrapper.c
@@ -6,7 +6,7 @@
 /*   By: tnaito <tnaito@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 19:04:04 by tnaito            #+#    #+#             */
-/*   Updated: 2025/10/24 21:27:44 by tnaito           ###   ########.fr       */
+/*   Updated: 2025/10/24 21:31:41 by tnaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ bool	rlwp_add_history(const char *line)
 {
 	if (line == NULL)
 		return (false);
-	if ((ft_strlen(line) == 1) && (line[0] == '\0'))
+	if ((ft_strlen(line) == 1) && (line[0] == '\n'))
 		return (false);
 	add_history(line);
 	return (true);
